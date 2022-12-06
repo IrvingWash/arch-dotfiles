051ac860e572bf5f7effe3ee69d27cbc836ee5ca
Washington Irving <iamwashingyou@gmail.com>
2022-12-06 22:52:54 +0300

    use i3 lock

---
+++ i3lock-color/i3lock-color.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#FFFFFF22'
+DEFAULT='#FFD47E'
+TEXT='#F5888A'
+WRONG='#FF6D83'
+VERIFYING='#CEEA9F'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 10                    \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"    \
+--keylayout 1                \
